c6a724254f71f519a5f80994d46711b056e23d53
Bob Park <hwpark11@kakao.com>
2025-01-25 15:16:34 +0900

    feat: 기타 필요한거 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,48 @@
 import type { Metadata } from 'next';
 
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import RQProvider from '@/app/_component/RQProvider';
+
 import './globals.css';
+
+const { WEB_SERVICE_HOST } = process.env;
 
 export const metadata: Metadata = {
   title: 'On Time ',
   description: '전자 근태 관리 시스템',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+
+  const apiResponse = await fetch(`${WEB_SERVICE_HOST}/users/me`, {
+    method: 'get',
+    headers: {
+      Cookie: `JSESSIONID=${cookieStore.get('JSESSIONID')?.value || ''}`,
+    },
+    credentials: 'include',
+  });
+
+  if (!apiResponse.ok) {
+    redirect('/api/oauth2/authorization/keyflow-auth');
+  }
+
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      {process.env.NODE_ENV !== 'production' && (
+        <head>
+          <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+        </head>
+      )}
+      <body className="">
+        <RQProvider>{children}</RQProvider>
+      </body>
     </html>
   );
 }
---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+}
