a3756f030248b66630bf129d3f6bf1e1921daa40
Bob Park <hwpark@malgn.com>
2025-01-31 16:13:07 +0900

    fix: 새로고침할때만 qr 이 나오는 버그 수정

--- src/app/(afterlogin)/(manager)/qr/_components/QRContents.tsx
+++ src/app/(afterlogin)/(manager)/qr/_components/QRContents.tsx
@@ -7,7 +7,7 @@ import { getDaysOfWeek } from '@/utils/parse';
 import { useGenerateCurrentCheck, useGetCurrentCheck } from '@/domain/attendance/query/attendanceCheck';
 import cx from 'classnames';
 import dayjs from 'dayjs';
-import QRCodeStyling from 'qr-code-styling';
+import QRCodeStyling, { Options } from 'qr-code-styling';
 
 const QR_CANVAS_ID = 'qr_canvas';
 
@@ -18,23 +18,64 @@ export default function QRContents() {
   // state
   const [selectType, setSelectType] = useState<AttendanceType>('CLOCK_IN');
 
+  const [qrOptions, setQrOptions] = useState<Options>({
+    width: 300,
+    height: 300,
+    image: '/malgn_logo.png',
+    dotsOptions: {
+      color: '#4267b2',
+      type: 'rounded',
+    },
+    imageOptions: {
+      crossOrigin: 'anonymous',
+    },
+  });
+  const [qrCode, setQrCode] = useState<QRCodeStyling>();
+
   // query
   const { currentCheck } = useGetCurrentCheck();
   const { generateCheck, isLoading } = useGenerateCurrentCheck();
 
   // useEffect
-
   useEffect(() => {
     generateCheck({ type: 'QR', attendanceType: selectType });
-  }, [selectType]);
+
+    setQrCode(new QRCodeStyling(qrOptions));
+  }, []);
 
   useEffect(() => {
     if (!currentCheck) {
       return;
     }
 
-    generateQrCode(currentCheck.id);
+    const url = `${location.origin}/attendance/record/${currentCheck?.id}`;
+    console.log(url);
+
+    setQrOptions({
+      ...qrOptions,
+      data: url,
+    });
   }, [currentCheck]);
+
+  useEffect(() => {
+    generateCheck({ type: 'QR', attendanceType: selectType });
+  }, [selectType]);
+
+  useEffect(() => {
+    console.log(qrCanvasRef.current);
+
+    if (!qrCanvasRef.current) {
+      return;
+    }
+
+    qrCode?.append(qrCanvasRef.current);
+
+    console.log('created qr');
+  }, [qrCode]);
+
+  useEffect(() => {
+    qrCode?.update(qrOptions);
+  }, [qrCode, qrOptions]);
 
   useEffect(() => {
     if (!currentCheck) {
@@ -55,32 +96,6 @@ export default function QRContents() {
   // handle
   const handleChangeSelectType = (type: AttendanceType) => {
     setSelectType(type);
-  };
-
-  const generateQrCode = (checkId: string) => {
-    if (!qrCanvasRef.current) {
-      return;
-    }
-
-    const url = `${location.origin}/attendance/record/${checkId}`;
-
-    const qrCode = new QRCodeStyling({
-      width: 300,
-      height: 300,
-      data: url,
-      image: '/malgn_logo.png',
-      dotsOptions: {
-        color: '#4267b2',
-        type: 'rounded',
-      },
-      imageOptions: {
-        crossOrigin: 'anonymous',
-      },
-    });
-
-    qrCode.append(qrCanvasRef.current);
-
-    console.log(url);
   };
 
   return (
@@ -108,29 +123,31 @@ export default function QRContents() {
       )}
 
       {/* QR Code */}
-      {!isLoading && currentCheck && (
-        <div className="mt-10 flex flex-col items-center justify-center gap-3">
-          <div className="flex flex-col items-center justify-start gap-1">
-            <div className="flex items-center justify-start gap-3">
-              <div className="flex-none text-right">근무일 : </div>
-              <div className="text-left">
-                <span>{dayjs(currentCheck.workingDate).format('YYYY년 MM월 DD일')}</span>
-                <span className="ml-1">({getDaysOfWeek(dayjs(currentCheck.workingDate).day())})</span>
-              </div>
-            </div>
-            <div className="flex items-center justify-start gap-3">
-              <div className="flex-none text-right">생성일 : </div>
-              <div className="text-left">{dayjs(currentCheck.createdDate).format('YYYY-MM-DD HH:mm:ss')}</div>
-            </div>
-            <div className="flex items-center justify-start gap-3">
-              <div className="flex-none text-right">만료일 : </div>
-              <div className="text-left">{dayjs(currentCheck.expiredDate).format('YYYY-MM-DD HH:mm:ss')}</div>
-            </div>
-          </div>
 
-          <div id={QR_CANVAS_ID} ref={qrCanvasRef} className="" />
+      <div className="mt-10 flex flex-col items-center justify-center gap-3">
+        <div className="flex flex-col items-center justify-start gap-1">
+          {!isLoading && currentCheck && (
+            <>
+              <div className="flex items-center justify-start gap-3">
+                <div className="flex-none text-right">근무일 : </div>
+                <div className="text-left">
+                  <span>{dayjs(currentCheck.workingDate).format('YYYY년 MM월 DD일')}</span>
+                  <span className="ml-1">({getDaysOfWeek(dayjs(currentCheck.workingDate).day())})</span>
+                </div>
+              </div>
+              <div className="flex items-center justify-start gap-3">
+                <div className="flex-none text-right">생성일 : </div>
+                <div className="text-left">{dayjs(currentCheck.createdDate).format('YYYY-MM-DD HH:mm:ss')}</div>
+              </div>
+              <div className="flex items-center justify-start gap-3">
+                <div className="flex-none text-right">만료일 : </div>
+                <div className="text-left">{dayjs(currentCheck.expiredDate).format('YYYY-MM-DD HH:mm:ss')}</div>
+              </div>
+            </>
+          )}
         </div>
-      )}
+        <div id={QR_CANVAS_ID} ref={qrCanvasRef} className={cx(isLoading && 'invisible')} />
+      </div>
     </div>
   );
 }
