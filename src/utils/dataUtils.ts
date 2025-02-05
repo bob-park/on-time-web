09338481209c12a66ccd5d1fe170f16123ce90e9
Bob Park <hwpark@malgn.com>
2025-02-05 17:21:05 +0900

    refactor: console.log 제거

--- src/utils/dataUtils.ts
+++ src/utils/dataUtils.ts
@@ -12,7 +12,5 @@ export function isSameMarginOfError(source: number, target: number, margin: numb
   const sourceInt = Math.round(source * Math.pow(10, pow));
   const targetInt = Math.round(target * Math.pow(10, pow));
 
-  console.log(marginInt, sourceInt, targetInt);
-
   return targetInt >= sourceInt - marginInt && targetInt <= sourceInt + marginInt;
 }
