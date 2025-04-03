8215dd91cc6ddc1856133a23b0171114d0fc4d58
Bob Park <hwpark11@kakao.com>
2025-04-04 00:22:15 +0900

    fix: eslint 적용

--- src/domain/document/api/documents.ts
+++ src/domain/document/api/documents.ts
@@ -1,7 +1,5 @@
 import api from '@/shared/api';
 
-import delay from '@/utils/delay';
-
 export async function searchDocument(req: SearchDocumentRequest) {
   const result = await api.get(`/api/documents`, { searchParams: req }).json<Page<Document>>();
 
