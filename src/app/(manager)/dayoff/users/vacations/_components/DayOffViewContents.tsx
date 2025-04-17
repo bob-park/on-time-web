8383128fbb10cfbcb30a4aea3d2a96b32e7681f0
Bob Park <hwpark11@kakao.com>
2025-04-17 22:46:34 +0900

    feat: hover 추가

--- src/app/(manager)/dayoff/users/vacations/_components/DayOffViewContents.tsx
+++ src/app/(manager)/dayoff/users/vacations/_components/DayOffViewContents.tsx
@@ -10,8 +10,8 @@ interface DayOffViewContentsProps {
 
 export default function DayOffViewContents({ order, user, usedVacations }: DayOffViewContentsProps) {
   return (
-    <div className="relative flex h-12 w-max flex-row items-center gap-2 rounded-2xl font-semibold transition-all duration-150 select-none">
-      <div className="sticky left-0 z-10 flex flex-row items-center gap-2 bg-white/90 backdrop-blur-xl">
+    <div className="hover:bg-base-300 relative flex h-12 w-max flex-row items-center gap-2 rounded-2xl font-semibold transition-all duration-150 select-none">
+      <div className="hover:bg-base-300 sticky left-0 z-10 flex h-full flex-row items-center gap-2 rounded-l-2xl backdrop-blur-2xl">
         <div className="w-12 flex-none text-center">{order}</div>
         <div className="w-20 flex-none text-center">{user.username}</div>
       </div>
