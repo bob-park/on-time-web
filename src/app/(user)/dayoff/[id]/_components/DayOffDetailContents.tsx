e3150b7dcfce2cde38b8610d400d124bfe26b36a
Bob Park <hwpark@malgn.com>
2025-04-18 13:02:01 +0900

    feat: 결재 재촉하기 버튼 기능 추가

--- src/app/(user)/dayoff/[id]/_components/DayOffDetailContents.tsx
+++ src/app/(user)/dayoff/[id]/_components/DayOffDetailContents.tsx
@@ -6,6 +6,8 @@ import { FaFilePdf } from 'react-icons/fa';
 import { GiCancel } from 'react-icons/gi';
 import { IoNotifications } from 'react-icons/io5';
 
+import PressApprovalModal from '@/app/(user)/dayoff/[id]/_components/PressApprovalModal';
+
 import ApprovalLines from '@/domain/approval/components/ApprovalLines';
 import VacationDocument from '@/domain/document/components/VacationDocument';
 import { useVacationDocument } from '@/domain/document/query/vacation';
@@ -25,6 +27,7 @@ const DEFAULT_VACATION_DOCUMENT_TAG_ID = 'vacation_document_id';
 export default function DayOffDetailContents({ id }: DayOffDetailContentsProps) {
   // state
   const [isPdfLoading, setIsPdfLoading] = useState<boolean>(false);
+  const [showPress, setShowPress] = useState<boolean>(false);
 
   // query
   const { vacationDocument } = useVacationDocument(id);
@@ -54,73 +57,88 @@ export default function DayOffDetailContents({ id }: DayOffDetailContentsProps) 
   };
 
   return (
-    <div className="flex size-full flex-col items-center justify-center gap-4">
-      {/* 현재 결재 라인 상태 정보 */}
-      <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
-        {/* body */}
-        <div className="card-body w-full">
-          <div className="flex w-full flex-col gap-4">
-            <div className="">
-              <h3 className="text-lg font-medium">현재 결재 상태</h3>
+    <>
+      <div className="flex size-full flex-col items-center justify-center gap-4">
+        {/* 현재 결재 라인 상태 정보 */}
+        <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
+          {/* body */}
+          <div className="card-body w-full">
+            <div className="flex w-full flex-col gap-4">
+              <div className="">
+                <h3 className="text-lg font-medium">현재 결재 상태</h3>
+              </div>
+              <div className="h-24 w-full">
+                <ApprovalLines
+                  lines={
+                    vacationDocument?.approvalHistories.map((item) => ({
+                      id: item.approvalLine.id,
+                      contents: item.approvalLine.contents,
+                      status: item.status || 'NOT_YET',
+                      reason: item.reason,
+                    })) || []
+                  }
+                />
+              </div>
             </div>
-            <div className="h-24 w-full">
-              <ApprovalLines
-                lines={
-                  vacationDocument?.approvalHistories.map((item) => ({
-                    id: item.approvalLine.id,
-                    contents: item.approvalLine.contents,
-                    status: item.status || 'NOT_YET',
-                    reason: item.reason,
-                  })) || []
-                }
-              />
+          </div>
+        </div>
+
+        {/* 휴가계 다운로드 버튼 */}
+        <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
+          <div className="card-body w-full">
+            <div className="mt-2 flex flex-col items-center justify-center gap-4">
+              <div className="flex w-full flex-row items-center justify-center gap-10">
+                <button type="button" className="btn btn-secondary w-full flex-1">
+                  <GiCancel className="size-6" />
+                  취소
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-soft btn-warning w-full flex-1"
+                  disabled={vacationDocument?.status !== 'WAITING'}
+                  onClick={() => setShowPress(true)}
+                >
+                  <IoNotifications className="size-6" />
+                  결재 재촉하기
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-neutral w-full flex-1"
+                  disabled={isPdfLoading}
+                  onClick={handlePdfDownloadClick}
+                >
+                  {isPdfLoading ? (
+                    <>
+                      <span className="loading loading-spinner" />
+                      생성중
+                    </>
+                  ) : (
+                    <>
+                      <FaFilePdf className="size-6" />
+                      PDF 다운로드
+                    </>
+                  )}
+                </button>
+              </div>
             </div>
+          </div>
+        </div>
+
+        {/* 휴가계 정보 */}
+        <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
+          <div className="mt-2 aspect-[1/1.414] w-[1000px]">
+            {vacationDocument && <VacationDocument id={DEFAULT_VACATION_DOCUMENT_TAG_ID} document={vacationDocument} />}
           </div>
         </div>
       </div>
 
-      {/* 휴가계 다운로드 버튼 */}
-      <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
-        <div className="card-body w-full">
-          <div className="mt-2 flex flex-col items-center justify-center gap-4">
-            <div className="flex w-full flex-row items-center justify-center gap-10">
-              <button type="button" className="btn btn-secondary w-full flex-1">
-                <GiCancel className="size-6" />
-                취소
-              </button>
-              <button type="button" className="btn btn-soft btn-warning w-full flex-1">
-                <IoNotifications className="size-6" />
-                결재 재촉하기
-              </button>
-              <button
-                type="button"
-                className="btn btn-neutral w-full flex-1"
-                disabled={isPdfLoading}
-                onClick={handlePdfDownloadClick}
-              >
-                {isPdfLoading ? (
-                  <>
-                    <span className="loading loading-spinner" />
-                    생성중
-                  </>
-                ) : (
-                  <>
-                    <FaFilePdf className="size-6" />
-                    PDF 다운로드
-                  </>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* 휴가계 정보 */}
-      <div className="card bg-base-100 m-3 flex w-full flex-col items-center justify-center gap-4 shadow-xl">
-        <div className="mt-2 aspect-[1/1.414] w-[1000px]">
-          {vacationDocument && <VacationDocument id={DEFAULT_VACATION_DOCUMENT_TAG_ID} document={vacationDocument} />}
-        </div>
-      </div>
-    </div>
+      <PressApprovalModal
+        show={showPress}
+        approvalUserUniqueId={
+          vacationDocument?.approvalHistories.find((item) => item.status === 'WAITING')?.approvalLine.userUniqueId
+        }
+        onClose={() => setShowPress(false)}
+      />
+    </>
   );
 }
---
+++ src/app/(user)/dayoff/[id]/_components/PressApprovalModal.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+import { FaCheck, FaTimes } from 'react-icons/fa';
+
+import { useGetCurrentUser } from '@/domain/user/query/user';
+import { useUserNotification } from '@/domain/user/query/userNotification';
+
+import useToast from '@/shared/hooks/useToast';
+
+interface PressApprovalModalProps {
+  show: boolean;
+  approvalUserUniqueId?: string;
+  onClose?: () => void;
+}
+
+export default function PressApprovalModal({ show, approvalUserUniqueId, onClose }: PressApprovalModalProps) {
+  // ref
+  const ref = useRef<HTMLDialogElement>(null);
+
+  // hooks
+  const { push } = useToast();
+
+  // query
+  const { currentUser } = useGetCurrentUser();
+  const { sendMessage, isLoading } = useUserNotification(
+    () => {
+      handleClose();
+      push('결재 재촉하였습니다.', 'info');
+    },
+    () => {
+      push('머가 잘못되었는디?', 'error');
+    },
+  );
+
+  // useEffect
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    show ? ref.current.showModal() : ref.current.close();
+  }, [show]);
+
+  // handle
+  const handleClose = () => {
+    onClose && onClose();
+  };
+
+  const handleKeyboardDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.key === 'Escape') {
+      handleClose();
+    }
+  };
+
+  const handlePressApproval = () => {
+    if (!approvalUserUniqueId || !currentUser) {
+      return;
+    }
+
+    sendMessage({
+      userUniqueId: approvalUserUniqueId,
+      body: {
+        displayMessage: `${currentUser.team?.name || ''} ${currentUser.username} ${currentUser.position?.name || ''} 이(가) 빨리 결재해달라고 재촉하였습니다.`,
+        fields: [],
+      },
+    });
+  };
+
+  return (
+    <dialog ref={ref} className="modal" onKeyDownCapture={handleKeyboardDown}>
+      <div className="modal-box">
+        <div className="flex w-full flex-col items-start justify-start gap-3">
+          {/* header */}
+          <div className="">
+            <h3 className="text-lg font-bold">결재 재촉하기</h3>
+          </div>
+        </div>
+
+        {/* content */}
+        <div className="m-3 flex flex-col items-start justify-center gap-4">
+          <p className="text-base">이시간 이후 결재자로부터 보복이 있을 수 있습니다.</p>
+          <p className="text-base font-semibold text-red-400">그래도 재촉하시겠습니까?</p>
+        </div>
+
+        {/* action */}
+        <div className="modal-action">
+          <button className="btn w-32" onClick={handleClose}>
+            <FaTimes className="size-6" />
+            안할까?
+          </button>
+          <button className="btn btn-primary w-36" disabled={isLoading} onClick={handlePressApproval}>
+            {isLoading ? (
+              <>
+                <span className="loading loading-spinner loading-xs" />
+                재촉중
+              </>
+            ) : (
+              <>
+                <FaCheck className="size-5" />
+                그래도 할꺼?
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+}
